6d7d318f9f30514c85d2573a26421cbbd2ec9d94
Kevin Lee <kevinsangholee@gmail.com>
2017-07-05 11:41:10 -0700

    Added worker

--- main.go
+++ main.go
@@ -23,33 +23,34 @@ func main() {
 			log.Println("Working on daily...")
 			time.Sleep(time.Minute)
 		}
-	}
-	router := gin.Default()
-	router.LoadHTMLFiles("templates/index.html", "templates/foot.html", "templates/head.html", "templates/food_cell.html")
-	router.Static("/css", "./css")
-	router.Static("/js", "./js")
-	router.Static("/img", "./img")
+	} else {
+		router := gin.Default()
+		router.LoadHTMLFiles("templates/index.html", "templates/foot.html", "templates/head.html", "templates/food_cell.html")
+		router.Static("/css", "./css")
+		router.Static("/js", "./js")
+		router.Static("/img", "./img")
 
-	// Open Connection
-	dsn := DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+		// Open Connection
+		dsn := DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME
+		db, err := sql.Open("mysql", dsn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
 
-	router.GET("/", func(c *gin.Context) {
-		foodMap := GetDaily(db)
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"foodData": foodMap,
+		router.GET("/", func(c *gin.Context) {
+			foodMap := GetDaily(db)
+			c.HTML(http.StatusOK, "index.html", gin.H{
+				"foodData": foodMap,
+			})
 		})
-	})
 
-	router.GET("/getReviews/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		reviews := GetReviews(db, id)
-		c.JSON(http.StatusOK, reviews)
-	})
+		router.GET("/getReviews/:id", func(c *gin.Context) {
+			id := c.Param("id")
+			reviews := GetReviews(db, id)
+			c.JSON(http.StatusOK, reviews)
+		})
 
-	router.Run(":" + port)
+		router.Run(":" + port)
+	}
 }
