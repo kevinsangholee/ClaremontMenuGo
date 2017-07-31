807981289211b989ef05cb8320f8aeee1c72068f
Kevin Lee <kevinsangholee@gmail.com>
2017-07-31 13:58:23 -0700

    Added badges for mobile app download

--- cmd/CMenu/main.go
+++ cmd/CMenu/main.go
@@ -7,16 +7,15 @@ import (
 	"log"
 	"database/sql"
 	"github.com/kevinsangholee/ClaremontMenuGo"
-	"os"
 )
 
 func main() {
 
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+	//port := os.Getenv("PORT")
+	//
+	//if port == "" {
+	//	log.Fatal("$PORT must be set")
+	//}
 
 	router := gin.Default()
 	router.LoadHTMLFiles("templates/index.html", "templates/foot.html", "templates/head.html", "templates/food_cell.html",
@@ -55,5 +54,6 @@ func main() {
 		c.JSON(http.StatusOK, reviews)
 	})
 
-	router.Run(":" + port)
+	//router.Run(":" + port)
+	router.Run(":8080")
 }
