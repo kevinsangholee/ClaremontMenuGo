f2819638637774f9c4a6d76abadda6e8f9bbbc78
Kevin Lee <kevinsangholee@gmail.com>
2017-07-31 16:25:28 -0700

    Ugly af but added daily support for image updating

--- cmd/daily/daily.go
+++ cmd/daily/daily.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"log"
-	"github.com/kevinsangholee/ClaremontMenuGo"
 	_ "github.com/go-sql-driver/mysql"
-	"database/sql"
 	"net/http"
-	"net/url"
 	"encoding/json"
 	"time"
-	"strings"
+	"net/url"
+	"github.com/kevinsangholee/ClaremontMenuGo"
+	"database/sql"
 	"strconv"
+	"strings"
 )
 
 type Menu struct {
@@ -18,6 +18,15 @@ type Menu struct {
 	Meal	   string   `json:"meal"`
 	FoodItems  []string `json:"food_items"`
 }
+
+type SingleBing struct {
+	ContentUrl string `json:"contentUrl"`
+}
+
+type BingResult struct {
+	Values []SingleBing `json:"value"`
+}
+
 
 const apiURL = "https://aspc.pomona.edu/api/menu/day/"
 
@@ -31,6 +40,9 @@ func main() {
 	values := url.Values {
 		"auth_token": []string{"2a9c963d749d6de4933579c611723625b74521c0"},
 	}
+
+	// Set HTTP Client for Bing
+	client := &http.Client{Timeout: 10 * time.Second}
 
 	// Get Request for current day
 	resp, err := http.Get(apiURL + day + "/?" + values.Encode())
@@ -73,20 +85,22 @@ func main() {
 		mealInt := mealToInt(menuItem.Meal)
 		for _, foodName := range menuItem.FoodItems {
 			// See if this item is in the database already
-			query := "SELECT daily, id FROM foods WHERE school = " + strconv.Itoa(schoolInt) +
+			query := "SELECT daily, id, image FROM foods WHERE school = " + strconv.Itoa(schoolInt) +
 				 " AND name = \"" + foodName + "\""
 			row := db.QueryRow(query)
 			var daily string
 			var id int
-			err := row.Scan(&daily, &id)
-			// If it doesn't exist, add it to the database
+			var image string
+			err := row.Scan(&daily, &id, &image)
+			// If it doesn't exist, find the image for it add it to the database
 			if err != nil {
 				if err == sql.ErrNoRows {
-					stmt, err := db.Prepare("INSERT INTO foods (name, school, image, daily) VALUES (?,?,'null',?)")
+					imageUrl := queryBingImageSearch(client, foodName)
+					stmt, err := db.Prepare("INSERT INTO foods (name, school, image, daily) VALUES (?,?,?,?)")
 					if err != nil {
 						log.Fatal(err)
 					}
-					_, err = stmt.Exec(foodName, schoolInt, "." + strconv.Itoa(mealInt))
+					_, err = stmt.Exec(foodName, schoolInt, imageUrl, "." + strconv.Itoa(mealInt))
 					if err != nil {
 						log.Fatal(err)
 					}
@@ -94,21 +108,70 @@ func main() {
 					log.Fatal(err)
 				}
 				log.Println("Added new food " + foodName + "!")
-			// If it does exist, just update the daily
+			// If it does exist, first check to see if there is an image for it.
 			} else {
 				newDaily := daily + strconv.Itoa(mealInt)
-				stmt, err := db.Prepare("UPDATE foods SET daily = ? WHERE id = ?")
-				if err != nil {
-					log.Fatal(err)
+				// If not, update daily with new image url
+				if image == "null" {
+
+					stmt, err := db.Prepare("UPDATE foods SET daily = ?, image = ? WHERE id = ?")
+					if err != nil {
+						log.Fatal(err)
+					}
+					imageUrl := queryBingImageSearch(client, foodName)
+					_, err = stmt.Exec(newDaily, imageUrl, id)
+					if err != nil {
+						log.Fatal(err)
+					}
+					log.Println("Updated the daily for " + foodName + "as well as updated the image!")
+				// If yes, just update daily
+				} else {
+
+					stmt, err := db.Prepare("UPDATE foods SET daily = ? WHERE id = ?")
+					if err != nil {
+						log.Fatal(err)
+					}
+					_, err = stmt.Exec(newDaily, id)
+					if err != nil {
+						log.Fatal(err)
+					}
+					log.Println("Updated the daily for " + foodName + "!")
+
 				}
-				_, err = stmt.Exec(newDaily, id)
-				if err != nil {
-					log.Fatal(err)
-				}
-				log.Println("Updated the daily for " + foodName + "!")
 			}
 		}
 	}
+}
+
+func queryBingImageSearch(client *http.Client, foodName string) string {
+
+	values := url.Values {
+		"q": []string{foodName},
+		"size": []string{"Medium"},
+	}
+
+	req, err := http.NewRequest("GET", "https://api.cognitive.microsoft.com/bing/v5.0/images/search/?" + values.Encode(), nil)
+	if err != nil {
+		log.Println(err)
+	}
+	req.Header.Set("Content-Type", "multipart/form-data")
+	req.Header.Set("Ocp-Apim-Subscription-Key", "6229f6930bc84d4a89a29215b1dc9f47")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+	}
+	defer resp.Body.Close();
+
+	data := BingResult{}
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data.Values[0].ContentUrl
+
 }
 
 func schoolToInt(school string) int {
